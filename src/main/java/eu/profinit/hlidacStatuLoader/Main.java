20f44d6101aa2792c039d9158b56c999210ed80c
Lucie Svitáková <lsvitakova@profinit.eu>
2019-02-19 10:05:29 +0100

    SFDI dataset added

--- src/main/java/eu/profinit/hlidacStatuLoader/Main.java
+++ src/main/java/eu/profinit/hlidacStatuLoader/Main.java
@@ -6,6 +6,7 @@ import eu.profinit.hlidacStatuLoader.institution.mdcr.MDCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mfcr.MFCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mmrcr.MMRCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mscr.MSCRCreator;
+import eu.profinit.hlidacStatuLoader.institution.sfdi.SFDICreator;
 import eu.profinit.hlidacStatuLoader.restCommunication.RestCommunicator;
 
 import java.io.IOException;
@@ -20,7 +21,8 @@ public class Main
 //        InstitutionCreator ic = new MFCRCreator();
 //        InstitutionCreator ic = new MSCRCreator();
 //        InstitutionCreator ic = new MMRCRCreator();
-        InstitutionCreator ic = new MDCRCreator();
+//        InstitutionCreator ic = new MDCRCreator();
+        InstitutionCreator ic = new SFDICreator();
         try {
             RestCommunicator.POSTRequest(DbCommunicator.getJsonResult(ic), ic.getDatasetId());
         } catch (IOException e) {
