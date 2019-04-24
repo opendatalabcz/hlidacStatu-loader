608b92ccb28ff4c2bb1b6c18575163b619d67c63
Lucie Svitáková <lsvitakova@profinit.eu>
2019-04-24 15:22:14 +0200

    52764 - added mocr

--- src/main/java/eu/profinit/hlidacStatuLoader/Main.java
+++ src/main/java/eu/profinit/hlidacStatuLoader/Main.java
@@ -5,6 +5,7 @@ import eu.profinit.hlidacStatuLoader.institution.InstitutionCreator;
 import eu.profinit.hlidacStatuLoader.institution.mdcr.MDCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mfcr.MFCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mmrcr.MMRCRCreator;
+import eu.profinit.hlidacStatuLoader.institution.mocr.MOCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mpocr.MPOCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mscr.MSCRCreator;
 import eu.profinit.hlidacStatuLoader.institution.mvcr.MVCRCreator;
@@ -28,7 +29,8 @@ public class Main
 //        InstitutionCreator ic = new SFDICreator();
 //        InstitutionCreator ic = new MVCRCreator();
 //        InstitutionCreator ic = new MPOCRCreator();
-        InstitutionCreator ic = new MZPCRCreator();
+//        InstitutionCreator ic = new MZPCRCreator();
+        InstitutionCreator ic = new MOCRCreator();
         try {
             RestCommunicator.POSTRequest(DbCommunicator.getJsonResult(ic), ic.getDatasetId());
         } catch (IOException e) {
---
+++ src/main/java/eu/profinit/hlidacStatuLoader/institution/mocr/MOCRCreator.java
@@ -0,0 +1,56 @@
+package eu.profinit.hlidacStatuLoader.institution.mocr;
+
+import eu.profinit.hlidacStatuLoader.institution.InstitutionCreator;
+import org.json.JSONObject;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+
+public class MOCRCreator implements InstitutionCreator {
+    @Override
+    public JSONObject createJson(ResultSet rs, DecimalFormat ft) throws SQLException {
+        JSONObject json = new JSONObject();
+        JSONObject partner = new JSONObject();
+
+        partner.put("name", rs.getString("name"));
+        partner.put("ICO", rs.getString("ico"));
+
+        json.put("Id", rs.getString("master_id"));
+        json.put("recordType", rs.getString("record_type"));
+        json.put("subject", rs.getString("subject"));
+        json.put("dateCreated", rs.getString("date_created"));
+        json.put("currency", rs.getString("currency"));
+        json.put("originalCurrencyAmount", ft.format(Double.parseDouble(rs.getString("original_currency_amount"))));
+//                json.put("originalCurrencyAmount", rs.getString("original_currency_amount"));
+        json.put("amountCzk", ft.format(Double.parseDouble(rs.getString("amount_czk"))));
+//                json.put("amountCzk", rs.getString("amount_czk"));
+        json.put("partner", partner);
+        json.put("authorityIdentifier", rs.getString("authority_identifier"));
+
+        return json;
+    }
+
+    @Override
+    public String getQuery() {
+        return "select master_id, " +
+                "record_type, " +
+                "subject, " +
+                "date_created, " +
+                "currency, " +
+                "original_currency_amount, " +
+                "amount_czk, " +
+                "en.name, " +
+                "en.ico, " +
+                "authority_identifier " +
+                "from record " +
+                "left join entity as en on partner = entity_id " +
+                "where authority = (select entity_id from entity as en where en.name like 'Ministerstvo obrany ČR') " +
+                "and record_type in ('invoice', 'payment')";
+    }
+
+    @Override
+    public String getDatasetId() {
+        return "faktury-mocr";
+    }
+}
